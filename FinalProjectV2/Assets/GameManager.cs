39e806de9b2e34f5cb184d1cd13118e02f0a3ac1
TheLion10 <itzhak.galperin@mail.huji.ac.il>
2023-01-28 15:35:01 +0200

    Added animation to mushroom layer

--- FinalProjectV2/Assets/GameManager.cs
+++ FinalProjectV2/Assets/GameManager.cs
@@ -6,10 +6,16 @@ public class GameManager : MonoBehaviour
 {
     public GameObject terrainLayer;
     public GameObject grassLayer;
+    public GameObject mushroomLayer;
     
     public void LoadGrassLayer()
     {
         terrainLayer.SetActive(false);
         grassLayer.SetActive(true);
+    }
+    public void LoadMushroomLayer()
+    {
+        grassLayer.SetActive(false);
+        mushroomLayer.SetActive(true);
     }
 }
