32af3f9e24d94785776f8291a596aa2e047cade5
ShakedOrenstein <75037743+ShakedOrenstein@users.noreply.github.com>
2023-03-02 21:37:40 +0200

    Twinkled Paws

--- FinalProjectV2/Assets/Mushrooms/FootstepsGenerator.cs
+++ FinalProjectV2/Assets/Mushrooms/FootstepsGenerator.cs
@@ -1,17 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using System.Threading;
 
 public class FootstepsGenerator : MonoBehaviour
 {
+    public GameObject[] paws;
+    private int pawIdx = 0;
+
+    private int waitTime = 0;
     // Start is called before the first frame update
     void Start()
     {
-        foreach(Transform child in transform)
+        foreach (GameObject paw in paws)
         {
-            Thread.Sleep(1000);
-            child.gameObject.SetActive(true);
+            Invoke("Generate", waitTime);
+            waitTime += 2;
         }
+    }
+
+    void Generate()
+    {
+        paws[pawIdx].SetActive(true);
+        pawIdx++;
     }
 }
