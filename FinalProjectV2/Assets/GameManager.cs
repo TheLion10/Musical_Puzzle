d6aae6abe29ead56413b3e11578054f3c5c17dba
Shaked Orenstein <75037743+ShakedOrenstein@users.noreply.github.com>
2023-03-10 18:11:25 +0200

    Deactivate layers when removing objects

--- FinalProjectV2/Assets/GameManager.cs
+++ FinalProjectV2/Assets/GameManager.cs
@@ -6,45 +6,13 @@ public class GameManager : MonoBehaviour
 {
     public GameObject terrainLayer;
     public GameObject grassLayer;
-    public GameObject mushroomLayer;
-    public GameObject flowerLayer;
-    public GameObject treeLayer;
-    public GameObject waterLayer;
-    
+
     public void LoadGrassLayer()
     {
         Invoke("RemoveTerrainLayer", 10);
         grassLayer.SetActive(true);
     }
-    public void LoadMushroomLayer()
-    {
-        mushroomLayer.SetActive(true);
-        // Invoke("RemoveGrassLayer", 10);
-    }
-    public void LoadFlowerLayer()
-    {
-        flowerLayer.SetActive(true);
-        // Invoke("RemoveMushroomLayer", 10);
-    }
-    
-    public void LoadTreeLayer()
-    {
-        treeLayer.SetActive(true);
-    }
-    
-    public void LoadWaterLayer()
-    {
-        waterLayer.SetActive(true);
-    }
 
-    private void RemoveGrassLayer()
-    {
-        grassLayer.SetActive(false);
-    }
-    private void RemoveMushroomLayer()
-    {
-        mushroomLayer.SetActive(false);
-    }
     private void RemoveTerrainLayer()
     {
         terrainLayer.SetActive(false);
