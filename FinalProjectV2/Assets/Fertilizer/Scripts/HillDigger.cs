0a3478f94978e051de6c176d34a5ebf27e53220a
Shaked Orenstein <75037743+ShakedOrenstein@users.noreply.github.com>
2023-03-10 15:50:56 +0200

    Fixed trawler behavior

--- FinalProjectV2/Assets/Fertilizer/Scripts/HillDigger.cs
+++ FinalProjectV2/Assets/Fertilizer/Scripts/HillDigger.cs
@@ -6,23 +6,15 @@ public class HillDigger : MonoBehaviour
 {
     public GameObject player;
     public GameObject hill;
-    private int canDig = 0;
+    private bool canDig = true;
     private void OnCollisionEnter(Collision collision)
     {
-        if (canDig == 1 && collision.gameObject.CompareTag("Ground"))
+        if (canDig && collision.gameObject.CompareTag("Ground"))
         {
             hill.transform.position = new Vector3(transform.position.x, -0.05f, transform.position.z);
             hill.transform.eulerAngles = player.transform.eulerAngles - Vector3.right * 90;
             hill.SetActive(true);
-            canDig = 2;
-        }
-    }
-
-    public void EnableDigging()
-    {
-        if (canDig == 0)
-        {
-            canDig = 1;
+            canDig = false;
         }
     }
 }
